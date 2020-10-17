227dd9387f2b1fc2d97b01dba5d0f068a43384ca
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2020-10-17 07:43:43 -0700

    Update grading script for additive grading

--- grading.js
+++ grading.js
@@ -1,61 +1,80 @@
-//copy paste this first into inspect element console.
+// Copy paste this first into inspect element console.
+
+/*
+ * Returns an array of booleans such that the ith rubric item corresponds to
+ * whether the ith rubric item should be selected for the currently viewed
+ * submission. This currently assumes an additive rubric and that the last
+ * rubric item is an "Incorrect/blank" rubric item so that Gradescope marks the
+ * submission as graded.
+ */
 function score() {
-  const marked = Array.from(document.getElementsByName('questions[5011939][0][]')).map(e => e.checked);
-  // you must inspect element to swap out '3208544' with appropriate question #.
-  // Grading logic here. Return a rubric (ret) which is an array of trues/falses corresponding to which
-  // rubric items that need to be marked. In the example below, ans_mask means [A, B, C] is the correct answer.
-  // However, the rubric uses negative scoring, so you only apply a rubric item when the answer is wrong.
-  const ans_mask = [0, 1, 0];
+  const ANS_MASK = [0, 1, 0];
+
+  const checkboxes = Array.from(document.querySelectorAll("[id^='question_'] input[type=checkbox]"))
+  const marked = checkboxes.map((e) => e.checked);
+
   let ret = [];
-  let all_correct = true; // We have an all correct rubric item so Gradescope marks the submission as graded.
-  for (let i = 0; i < ans_mask.length; i++) {
-      if (ans_mask[i] != marked[i] | 0) {
+  let allIncorrect = true;
+  for (let i = 0; i < ANS_MASK.length; i++) {
+      if (ANS_MASK[i] == marked[i] || 0) {
           ret.push(true);
-          all_correct = false;
+          allIncorrect = false;
       } else {
           ret.push(false);
       }
   }
-  ret.push(all_correct);
+  ret.push(allIncorrect);
   return ret;
 }
-//run score() on console to see if it produces the correct grade.
+
+// Run score() on console to see if it produces the correct grade.
+
+// Next, copy paste this into the console.
+
+/*
+ * Grades the current submission based on the return value of score().
+ */
 function grade() {
-  let scored_rubric = score();
-  let rubric = document.getElementsByClassName('rubricItem--key');
-  if (scored_rubric[0]) {
-      rubric[0].click();
-  } else if (scored_rubric[2]) {
-      rubric[0].click();
-  } else {
-      rubric[1].click();
-  }
-  //go to next ungraded. This means whatever submission is already graded is unaffected by this script.
-  document.getElementsByClassName('actionBar--action-next')[0].click();
-}
-function grade() {
-  let scored_rubric = score();
-  let rubric = document.getElementsByClassName('rubricItem--key');
-  if (scored_rubric[scored_rubric.length - 1]) {
-      rubric[0].click();
-  } else {
-      rubric[1].click();
-  }
-  //go to next ungraded. This means whatever submission is already graded is unaffected by this script.
-  document.getElementsByClassName('actionBar--action-next')[0].click();
-}
-function grade() {
-  let scored_rubric = score();
-  let rubric = document.getElementsByClassName('rubricItem--key');
-  for (let i = 0; i < scored_rubric.length; i++) {
-      // Apply the item rubric[i] if scored_rubric[i] is true.
-      if (scored_rubric[i]) {
+  let scoredRubric = score();
+  let rubric = document.querySelectorAll('.rubricItem--key');
+  for (let i = 0; i < scoredRubric.length; i++) {
+      // Apply the item rubric[i] if scoredRubric[i] is true.
+      if (scoredRubric[i]) {
           rubric[i].click();
       }
   }
-  //go to next ungraded. This means whatever submission is already graded is unaffected by this script.
+
+  // Go to next ungraded. This means whatever submission is already graded is unaffected by this script.
   document.getElementsByClassName('actionBar--action-next')[0].click();
 }
-//run grade() on console to see if correct choice selected.
-//1000 = 1 sec. Change this accordingly based on how fast your browser/internet runs.
+// Run grade() on console to see if the correct choice is selected.
+
+// If it works, paste this into the console to start the autograding procedure.
+// 1000 = 1 sec. Change this accordingly based on how fast your browser/internet runs.
 setInterval(grade, 500);
+
+//function grade() {
+//  let scored_rubric = score();
+//  let rubric = document.getElementsByClassName('rubricItem--key');
+//  if (scored_rubric[0]) {
+//      rubric[0].click();
+//  } else if (scored_rubric[2]) {
+//      rubric[0].click();
+//  } else {
+//      rubric[1].click();
+//  }
+//  //go to next ungraded. This means whatever submission is already graded is unaffected by this script.
+//  document.getElementsByClassName('actionBar--action-next')[0].click();
+//}
+//
+//function grade() {
+//  let scored_rubric = score();
+//  let rubric = document.getElementsByClassName('rubricItem--key');
+//  if (scored_rubric[scored_rubric.length - 1]) {
+//      rubric[0].click();
+//  } else {
+//      rubric[1].click();
+//  }
+//  //go to next ungraded. This means whatever submission is already graded is unaffected by this script.
+//  document.getElementsByClassName('actionBar--action-next')[0].click();
+//}
